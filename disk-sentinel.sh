9600e5ad6564276e6414a8f22c9d28a81fa33604
pixatintes <pixatintes@yahoo.es>
2026-01-12 12:04:33 +0100

    Update disk-sentinel.sh

--- disk-sentinel.sh
+++ disk-sentinel.sh
@@ -1,23 +1,10 @@
-# Disk Sentinel – NO-WAKEUP Disk Standby Manager
-# Copyright (C) 2025  
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
 #!/bin/bash
 # ============================================================
-#   Disk Sentinel - Auto Standby NO-WAKEUP + Wake Detection
+#   Disk Sentinel – Instal·lador estàndard (NO-WAKEUP)
+#   Copyright (C) 2025
+#
+#   Aquest programa és programari lliure: podeu redistribuir-lo i/o
+#   modificar-lo sota els termes de la GNU GPL v3 o posterior.
 # ============================================================
 
 CONFIG_FILE="/usr/local/bin/disk-sentinel/config.conf"
